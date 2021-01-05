c9a79db995ccebd1eff20ebc9c7b4825f896cdf7
nwojcik <nwojcik@fis-sst.pl>
2021-01-05 03:08:46 +0100

    Implemented JWT auth, registration and login.

--- work-reporting-frontend/angular-app/src/app/app-routing.module.ts
+++ work-reporting-frontend/angular-app/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ListComponent } from './list/list.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { AuthGuard } from './auth.service';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'list', component: ListComponent, canActivate: [AuthGuard] },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
