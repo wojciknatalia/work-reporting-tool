50981194538176f9b316a006344c85e07f34acb6
nwojcik <nwojcik@fis-sst.pl>
2021-01-05 02:12:07 +0100

    Enabled adding tasks from frontend by form, implemented validators.

--- work-reporting-frontend/angular-app/src/app/task-list/task-list.component.ts
+++ work-reporting-frontend/angular-app/src/app/task-list/task-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ApiService } from '../api.service';
 import { Task } from '../task'
 
@@ -11,31 +12,36 @@ import { Task } from '../task'
 })
 export class TaskListComponent implements OnInit {
   tasks$: Observable<Task[]>;
+  task_form: FormGroup;
 
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService, private form_builder: FormBuilder) { }
 
   ngOnInit(): void {
     this.getTasks();
 
-    // this.task_form = this.form_builder.group({
-    //   title: '',
-    //   hours: '',
-    //   employee_email: this.authService.getEmail()
-    // });
+    this.task_form = this.form_builder.group({
+      title: '',
+      hours: '',
+      employee_email: 'useremail@gmail.com'
+    });
+
+    // Set validators for fields.
+    this.task_form.controls["title"].setValidators([Validators.required]);
+    this.task_form.controls["hours"].setValidators([Validators.required]);
   }
 
   public getTasks() {
     this.tasks$ = this.apiService.getTasks();
   }
 
-  // onSubmit() {
-  //   // Create the Task.
-  //   this.apiService.postTask(this.task_form.value)
-  //     .subscribe(
-  //       (response) => {
-  //         console.log(response);
-  //         this.getTasks();
-  //       }
-  //     )
-  // }
+  onSubmit() {
+    // Create the Task.
+    this.apiService.postTask(this.task_form.value)
+      .subscribe(
+        (response) => {
+          console.log(response);
+          this.getTasks();
+        }
+      )
+  }
 }
